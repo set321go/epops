b1773fc9530f44613eb1bac9045626d5aca23e42
Alex Edwards <edwards.alex@gmail.com>
2016-03-13 21:13:34 -0700

    fixed relative path problem with test directories

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -8,14 +8,14 @@ from core.extensions.loader import ExtensionLoader
 class TestExtensionLoader(TestCase):
 
     def test_init_no_extensions(self):
-        core_config = {'plugin_dir': [os.path.abspath("no-extensions")]}
+        core_config = {'plugin_dir': [os.path.join(os.path.curdir, 'tests', 'no-extensions')]}
         loader = ExtensionLoader(core_config)
         result = loader.ext_init()
 
         self.assertFalse(result)
 
     def test_init_with_extensions(self):
-        core_config = {'plugin_dir': [os.path.abspath("extensions")]}
+        core_config = {'plugin_dir': [os.path.join(os.path.curdir, 'tests', 'extensions')]}
         loader = ExtensionLoader(core_config)
         result = loader.ext_init()
 
@@ -23,7 +23,7 @@ class TestExtensionLoader(TestCase):
         self.assertEqual(1, len(result))
 
     def test_ext_load_new(self):
-        core_config = {'plugin_dir': [os.path.abspath("zip-extensions")]}
+        core_config = {'plugin_dir': [os.path.join(os.path.curdir, 'tests', 'zip-extensions')]}
         loader = ExtensionLoader(core_config)
         result = loader.ext_load_new('Archive.zip')
 
