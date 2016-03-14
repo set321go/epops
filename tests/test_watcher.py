8e56170b219ea2a529e88f4a312492b7753b1abf
Alex Edwards <edwards.alex@gmail.com>
2016-03-13 21:34:17 -0700

    more tests and fix for travis config

---
+++ tests/test_watcher.py
@@ -0,0 +1,20 @@
+import os
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from core.extensions.watcher import FileWatcher
+
+
+class TestFileWatcher(TestCase):
+
+    def setUp(self):
+        self.mock_manager = MagicMock()
+        self.watcher = FileWatcher(self.mock_manager)
+
+    def test_creat_event(self):
+        mock_event = MagicMock()
+        mock_event.src_path.return_value = 'test-path'
+
+        self.watcher.on_created(mock_event)
+
+        self.mock_manager.new_plugin.assert_called_with(os.path.basename(mock_event.src_path))
